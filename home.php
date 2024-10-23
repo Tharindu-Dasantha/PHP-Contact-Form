939edbfee98996b30638f15aa0033888f5bf02ab
Tharindu-Dasantha <fsintern1@hologo.world>
2024-10-23 15:47:20 +0530

    Added styles to the file. Completed the project.

--- home.php
+++ home.php
@@ -2,8 +2,9 @@
 <html>
     <head>
         <meta charset="UTF-8" />
-        <meta name="viewport" content="width=devie-width, initial-scale=1.0" />
-        <title>Gether Feedback</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Gathering Feedback</title>
+        <link rel="stylesheet" type="text/css" href="css/home.css">
     </head>
     <body>
         <h1>Gethering Feedback</h1>
--- php/feedbacks.php
+++ php/feedbacks.php
@@ -5,46 +5,39 @@ $data = getData();
 ?>
 
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta viewport="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Feedback</title>
-    </head>
-    <body>
-        <div>
-            <h1>Feedbacks form customers</h1>
-            <div>
-                <?php foreach ($data as $item):
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Feedback</title>
+    <link rel="stylesheet" type="text/css" href="../css/feedbacks.css">
+</head>
+<body>
+<div>
+    <h1 class="heading">Feedbacks form customers</h1>
+    <div class="container">
+        <?php foreach ($data as $item):
 
-                    $name = htmlspecialchars($item["name"]);
-                    $email = htmlspecialchars($item["email"]);
-                    $feedbackType = htmlspecialchars($item["feedback-type"]);
-                    $feedback = htmlspecialchars($item["feedback"]);
-                    ?>
+            $name = htmlspecialchars($item["name"]);
+            $email = htmlspecialchars($item["email"]);
+            $feedbackType = htmlspecialchars($item["feedback-type"]);
+            $feedback = htmlspecialchars($item["feedback"]);
+            ?>
 
-                    <!-- container for all the cards -->
-                    <div>
-                    <!-- card -->
-                        <div>
-                            <h3>
-                                <?php echo "$feedbackType"; ?>
-                            </h3>
-                            <p>
-                                <?php echo "$feedback"; ?>
-                            </p>
-                            <!-- name and email -->
-                            <div>
-                                <p><?php echo "$name"; ?></p>
-                                <p><?php echo "$email"; ?></p>
-                            </div>
-                        </div>
-                    </div>
-
-                <?php
-                endforeach; ?>
-
+            <div class="card">
+                <p class="feedback-type"><?php echo $feedbackType; ?></p>
+                <p class="feedback">"<?php echo $feedback; ?>"</p>
+                <div class="details">
+                    <p class="name"><?php echo $name; ?></p>
+                    <p class="email"><?php echo $email; ?></p>
+                </div>
             </div>
-        </div>
-    </body>
+
+        <?php
+        endforeach; ?>
+
+
+    </div>
+</div>
+</body>
 </html>
